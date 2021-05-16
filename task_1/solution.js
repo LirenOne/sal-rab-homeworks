fd532baf1a425d9c7c2ad19387ea21b26d574b4a
LirenOne <k_ukol_ka@bk.ru>
2021-05-16 19:36:36 +0300

    Исправление task_1 Толстова ЛВ

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,23 +5,23 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
-    let message
+    let message;
     // присвойте переменной message значение переменной productName
-    message = productName
+    message = productName;
     // добавьте в конец переменной message пробел
-    message = message + " "
+    message = message + " ";
     // добавьте в конец переменной message текст 'за'
-    message = message + "за"
+    message = message + "за";
     // добавьте в конец переменной message пробел
-    message = message + " "
+    message = message + " ";
     // добавьте в конец переменной message значение переменной productPrice
-    message = message + productPrice
+    message = message + productPrice;
     // добавьте в конец переменной message пробел
-    message = message + " "
+    message = message + " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
-    message = message + "теперь в корзине!"
+    message = message + "теперь в корзине!";
     // выведите переменную message в консоль
-    print(message)
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
